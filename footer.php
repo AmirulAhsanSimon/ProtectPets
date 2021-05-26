03baf3e96b6da44137d1af68fdf89202590c4c05
1632129642-Amirul <70319162+AmirulAhsanSimon@users.noreply.github.com>
2021-05-26 20:21:47 +0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,29 @@
 <footer>
-	<p class="p-3 bg-dark text-white text-center">Protect Pets<br>Developed by Amirul Ahsan  | | Farzana Rahman <br>Copyright &copy; 2021 Protec Pets</p>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+
+  <div class="container">
+    <div class="sec aboutus m-right">
+      <h2>About us</h2>
+      <p>We provided a website for animal welfare. Our goal is safe animals, safe ecosystem. This is a nonprofit project. </p>
+      <ul class="sci">
+      <li><a href="https://www.facebook.com/Protect-Pets-105525271471970"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+      <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+      <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+      <li><a href="#"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+    </ul>   
+    </div>
+    <div class="sec quickLinks m-auto">
+      <h2>Quick Links</h2>
+      <ul>
+        <li><a href="#">About</a></li>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Privacy Policy</a></li>
+        <li><a href="#">Helps</a></li>
+        <li><a href="#">Terms & Conditions</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+	<p class="p-2 bg-dark text-white text-center">Protect Pets<br>Developed by Amirul Ahsan  | | Farzana Rahman <br>Copyright &copy; 2021 Protect Pets</p>
 </footer>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>

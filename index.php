b881486fb124143ecdd996f3a84a659f70977407
1632129642-Amirul <70319162+AmirulAhsanSimon@users.noreply.github.com>
2021-05-26 15:42:27 +0600

    Update index.php

--- index.php
+++ index.php
@@ -1,86 +1,4 @@
-<?php 
-session_start();
-
-if (!$_SESSION['login']) {
-  header("Location: login.php");
-}
-
- ?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<meta charset="utf-8">
-	<title>Animal rescue & welfare</title>
-	<meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
-    <link rel="stylesheet" type="text/css" href="css/style1.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com"> 
-<link href="https://fonts.googleapis.com/css2?family=Nerko+One&display=swap" rel="stylesheet">
-</head>
-<?php 
-  if (isset($_GET['logout'])) {
-    session_destroy();
-    header("Location:login.php");
-  }
- ?>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container">
-      <a class="navbar-brand" href="#"><?php echo $_SESSION["username"]; ?></a>
-    </div>
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link btn btn-success" href="?logout">Logout</a>
-      </li>
-    </ul>
-  </nav>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">PROTECT PETS</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav ml-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        </li>
-        <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          RESCUE
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="regis.php">Post for rescue</a>
-          <a class="dropdown-item" href="#">News of rescue</a>
-      </li>
-       <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          INFORMATIONS
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="https://www.tutorialspoint.com/how-to-take-care-of-pets-at-home">How to take care pets</a>
-          <a class="dropdown-item" href="https://www.avma.org/resources-tools/pet-owners/petcare/vaccinations">About vaccine</a>
-          <a class="dropdown-item" href="https://petbangla.com/">About food store</a>
-          <a class="dropdown-item" href="https://www.pet.com.bd/vet-finder/">About vets</a>
-          <a class="dropdown-item" href="https://www.finddoctor24.com/dhaka-animal-hospital-list-location/">About hospitals</a>
-          <a class="dropdown-item" href="https://cvasu.ac.bd/">About study</a>
-      </li>
-        
-       
-        <li class="nav-item">
-          <a class="nav-link" href="regisv.php">Volunteer</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="thanks.php">Thanks</a>
-        </li>
-      </ul>
-      
-    </div>
-  </div>
-</nav>
+<?php include 'header.php'; ?>
 <div id="demo" class="carousel slide" data-ride="carousel">
   <ul class="carousel-indicators">
     <li data-target="#demo" data-slide-to="0" class="active"></li>
@@ -188,10 +106,5 @@ YOU WILL ALWAYS BE THERE IN OUR HEART STEVIE.</h4>
 </footer>
 
 <?php include 'footer.php'; ?>
-
-	
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>

c1c58b2212a9cd1c6512aeb73e020563b63d9583
1632129642-Amirul <70319162+AmirulAhsanSimon@users.noreply.github.com>
2021-05-28 08:27:29 +0600

    Add files via upload

---
+++ thanks.php
@@ -0,0 +1,11 @@
+<?php include 'header.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Thanks Page</title>
+</head>
+<body>
+<h1>Thanks for visiting our website!</h1>
+</body>
+</html>
